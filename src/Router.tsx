317f94517599e555f48247869af027ceb07e5849
Leonardo <leonardosilvalima93@live.com>
2023-05-17 14:49:51 -0300

    Layout de rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { CompleteOrderPage } from './pages/CompleteOrder'
 import { OrderConfirmedPage } from './pages/OrderConfirmed'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/completeOrder" element={<CompleteOrderPage />} />
-      <Route path="/orderConfirmed" element={<OrderConfirmedPage />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/completeOrder" element={<CompleteOrderPage />} />
+        <Route path="/orderConfirmed" element={<OrderConfirmedPage />} />
+      </Route>
     </Routes>
   )
 }
