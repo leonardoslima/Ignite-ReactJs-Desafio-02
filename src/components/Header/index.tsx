a1c6b92e3b9c72fd683ec258aeef63174e647d00
Leonardo <leonardosilvalima93@live.com>
2023-05-17 16:14:42 -0300

    Criando componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,27 @@
+import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from './styles'
+
+import logoCoffee from '../../assets/logo.svg'
+import { NavLink } from 'react-router-dom'
+import { MapPin, ShoppingCart } from '@phosphor-icons/react'
+
 export function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <HeaderContainer>
+      <NavLink to="/">
+        <img src={logoCoffee} alt="" />
+      </NavLink>
+
+      <HeaderButtonsContainer>
+        <HeaderButton variant={'yellow'}>
+          <MapPin size={20} weight="fill" />
+          Porto Alegre, RS
+        </HeaderButton>
+
+        <HeaderButton variant={'purple'}>
+          <span>10</span>
+          <ShoppingCart size={20} weight="fill" />
+        </HeaderButton>
+      </HeaderButtonsContainer>
+    </HeaderContainer>
   )
 }
---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  max-width: 74rem;
+  margin: auto;
+  padding: 2.5rem;
+
+  display: flex;
+  flex-direction: column;
+`
