1666e4df566986b644ec676b8227f420e3e2948b
Leonardo <leonardosilvalima93@live.com>
2023-05-24 12:08:15 -0300

    Modificações no DefaultLayout e novas imagens

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  max-width: 74rem;
-  margin: auto;
-  padding: 2.5rem;
-
-  display: flex;
-  flex-direction: column;
+  .container {
+    width: 100%;
+    max-width: 70rem;
+    margin-right: auto;
+    margin-left: auto;
+  }
 `
