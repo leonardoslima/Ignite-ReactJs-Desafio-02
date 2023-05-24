55a06099e5ba93351d4c0d5e6f81815dfce6d224
Leonardo <leonardosilvalima93@live.com>
2023-05-24 16:13:29 -0300

    Criando componente QuantityInput

---
+++ src/components/QuantityInput/index.tsx
@@ -0,0 +1,28 @@
+import { Minus, Plus } from '@phosphor-icons/react'
+import { QuantityInputContainer, IconWrapper } from './styles'
+
+interface QuantityInputProps {
+  size?: 'medium' | 'small'
+  quantity: number
+  onIncrease: () => void
+  onDecrease: () => void
+}
+
+export function QuantityInput({
+  quantity,
+  size = 'medium',
+  onIncrease,
+  onDecrease,
+}: QuantityInputProps) {
+  return (
+    <QuantityInputContainer size={size}>
+      <IconWrapper disabled={quantity <= 1} onClick={onDecrease}>
+        <Minus size={14} weight="fill" />
+      </IconWrapper>
+      <input type="number" readOnly value={quantity} />
+      <IconWrapper onClick={onIncrease}>
+        <Plus size={14} weight="fill" />
+      </IconWrapper>
+    </QuantityInputContainer>
+  )
+}
