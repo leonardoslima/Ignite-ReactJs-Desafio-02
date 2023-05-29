4123fc5182a5e83e9ab2556f6f350870faec06f0
Leonardo <leonardosilvalima93@live.com>
2023-05-29 13:41:26 -0300

    Finalizando layout da pagina de confirmação do pedido

--- src/pages/OrderConfirmed/index.tsx
+++ src/pages/OrderConfirmed/index.tsx
@@ -1,3 +1,59 @@
+import { Clock, CurrencyDollar, MapPin } from '@phosphor-icons/react'
+import { InfoWithIcon } from '../../components/InfoWithIcon'
+import { OrderConfirmedContainer, OrderDetailsContainer } from './styles'
+import { useTheme } from 'styled-components'
+
+import confirmedOrderIllustration from '../../assets/confirmed-order.svg'
+
 export function OrderConfirmedPage() {
-  return <h1>OrderConfirmed</h1>
+  const { colors } = useTheme()
+
+  return (
+    <OrderConfirmedContainer className="container">
+      <div>
+        <h1>Uhu! Pedido confirmado</h1>
+        <h4>Agora é só aguardar que logo o café chegará até você</h4>
+      </div>
+
+      <section>
+        <OrderDetailsContainer>
+          <InfoWithIcon
+            icon={<MapPin weight="fill" />}
+            iconBg={colors['brand-purple']}
+            text={
+              <p>
+                Entrega em <strong>Rua João Daniel Martinelli</strong>, 102
+                <br />
+                Farrapos - Porto Alegre, RS
+              </p>
+            }
+          />
+          <InfoWithIcon
+            icon={<Clock weight="fill" />}
+            iconBg={colors['brand-yellow']}
+            text={
+              <p>
+                Previsão de entrega
+                <br />
+                <strong>20 min - 30 min</strong>
+              </p>
+            }
+          />
+          <InfoWithIcon
+            icon={<CurrencyDollar weight="fill" />}
+            iconBg={colors['brand-yellow-dark']}
+            text={
+              <p>
+                Pagamento na entrega
+                <br />
+                <strong>Cartão de Crédito</strong>
+              </p>
+            }
+          />
+        </OrderDetailsContainer>
+
+        <img src={confirmedOrderIllustration} alt="" />
+      </section>
+    </OrderConfirmedContainer>
+  )
 }
